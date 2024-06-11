92e57e251d75212faf3fb1ebdc239aadf4bf507b
Josiel <josiel.borges@sc.senai.br>
2024-06-11 13:59:41 -0300

    Implemented the functionality of questioning the gpt

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
+from time import sleep
 
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from openai import OpenAI
 
 load_dotenv()
@@ -16,6 +17,52 @@ app.config.from_pyfile('config.py')
 @app.route('/')
 def home():
     return render_template("index.html")
+
+
+@app.route('/chat', methods=["POST"])
+def chat():
+    prompt = request.json['msg']
+    response = bot(prompt)
+    response_text = response.choices[0].message.content
+    return response_text
+
+
+def bot(prompt):
+    max_try = 1
+    i = 0
+
+    while True:
+        try:
+            system_prompt = f"""
+            Você é um chatbot de atendimento a clientes de um e-commerce. 
+            Você não deve responder perguntas que não sejam dados do e-commerce informado!
+            """
+
+            response = client.chat.completions.create(
+                messages=[
+                    {
+                        "role": "system",
+                        "content": system_prompt
+                    },
+                    {
+                        "role": "user",
+                        "content": prompt
+                    }
+                ],
+                temperature=1,
+                max_tokens=256,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0,
+                model=model
+            )
+            return response
+        except Exception as e:
+            i += 1
+            if i > max_try:
+                return "Erro no GPT: %s" % e
+            print('Erro de comunicação com OpenAI:', e)
+            sleep(1)
 
 
 if __name__ == '__main__':
