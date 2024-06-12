29d774c3c912ca785e869fa8822fb47ac2cd77cb
Josiel <josiel.borges@sc.senai.br>
2024-06-12 13:54:07 -0300

    Implementation of a function calling

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import os
 from time import sleep
 
@@ -7,6 +8,7 @@ from openai import OpenAI
 
 from assistant import get_json
 from persona_selection import personas, select_persona
+from tools import functions
 
 load_dotenv()
 
@@ -20,6 +22,9 @@ assistant = get_json()
 thread_id = assistant['thread_id']
 vector_store_id = assistant['vector_store_id']
 assistant_id = assistant['assistant_id']
+
+STATUS_COMPLETED = 'completed'
+STATUS_REQUIRES_ACTION = 'requires_action'
 
 
 @app.route('/')
@@ -68,11 +73,37 @@ def bot(prompt):
                 assistant_id=assistant_id
             )
 
-            while run.status != 'completed':
+            while run.status != STATUS_COMPLETED:
                 run = client.beta.threads.runs.retrieve(
                     thread_id=thread_id,
                     run_id=run.id
                 )
+
+                print(f"Status: {run.status}")
+
+                if run.status == STATUS_REQUIRES_ACTION:
+                    called_tools = run.required_action.submit_tool_outputs.tool_calls
+                    called_actions_responses = []
+
+                    for called_tool in called_tools:
+                        function_name = called_tool.function.name
+                        chosen_function = functions[function_name]
+                        arguments = json.loads(called_tool.function.arguments)
+                        print(arguments)
+                        function_response = chosen_function(arguments)
+
+                        called_actions_responses.append(
+                            {
+                                'tool_call_id': called_tool.id,
+                                'output': function_response
+                            }
+                        )
+
+                    run = client.beta.threads.runs.submit_tool_outputs(
+                        thread_id=thread_id,
+                        run_id=run.id,
+                        tool_outputs=called_actions_responses
+                    )
 
             history = list(client.beta.threads.messages.list(thread_id=thread_id).data)
             response = history[0]
--- tools.py
+++ tools.py
@@ -11,5 +11,47 @@ model = 'gpt-4-1106-preview'
 tools = [
     {
         'type': 'file_search'
+    },
+    # See more at https://cursos.alura.com.br/course/python-gpt-crie-chatbot-com-ia/task/148376
+    {
+        'type': 'function',
+        'function': {
+            'name': 'validate_promo_code',
+            'description': 'Valide um código promocional com base nas diretrizes de Descontos e Promoções da empresa',
+            'parameters': {
+                'type': 'object',
+                'properties': {
+                    'code': {
+                        'type': 'string',
+                        'description': 'O código promocional, no formato, CUPOM_XX. Por exemplo: CUPOM_ECO',
+                    },
+                    'expiration_date': {
+                        'type': 'string',
+                        'description': f'A validade do cupom, caso seja válido e esteja associado as políticas. '
+                                       f'No formato DD/MM/YYYY.',
+                    },
+                },
+                'required': ['code', 'expiration_date'],
+            }
+        }
     }
 ]
+
+
+def validate_promo_code(arguments):
+    print('Entrou na função')
+    code = arguments.get('code')
+    expiration_date = arguments.get('expiration_date')
+
+    return f'''
+        
+        # Formato de Resposta
+        
+        {code} com validade: {expiration_date}. 
+        Ainda, diga se é válido ou não para o usuário.
+        '''
+
+
+functions = {
+    'validate_promo_code': validate_promo_code
+}
